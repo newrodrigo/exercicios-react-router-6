73f4f270d2bc8f2ccb8876ccdfdbf61a0f1fc54f
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-19 22:24:57 -0300

    Ex1 end

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
 import Card from "../../../components/Card";
 
@@ -9,9 +10,10 @@ export default function HomeBody() {
           <div className="mt30 mb30">
             <Card title="Página inicial" />
           </div>
-
           <div className="dflex">
-            <Button text="Ver Promoção" />
+            <Link to="/promotion">
+              <Button text="Ver Promoção" />
+            </Link>
           </div>
         </div>
       </section>
